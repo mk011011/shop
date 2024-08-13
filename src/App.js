4f88c552d17e2f6420a3b8148db2e551e0ea1efd
ok63477 <ok63477@gmail.com>
2024-08-13 19:41:57 +0900

    원상태로!

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
   let navigate = useNavigate()
   let [shoesData, setShoesData] = useState(2)
   let [isDisable, setIsDisable] = useState(false)
-  let[이상한함수] =useState();
 
+  
   return (
     <div className="App">
 
